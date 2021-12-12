4183c875edd91aead86ca9b7ad217c2658ad5ff8
uwu-chan <91139990+pogboi69@users.noreply.github.com>
2021-12-12 20:50:30 +0530

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -9,7 +9,7 @@ module.exports = {
         const { channel } = message.member.voice;
         if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to change volume!');
         if (message.guild.me.voice.channel !== message.member.voice.channel) {
-            return message.channel.send("**You Have To Be In The Same Channel With The Bot!**");
+            return message.channel.send("**You Have To Be In The Same Voice Channel With The Bot!**");
           }
         const serverQueue = ops.queue.get(message.guild.id);
         if (!serverQueue) return message.channel.send('There is nothing playing.');
